221e7c8f09f5064ab3a16b849eadc7e19251e347
gprimo <glaucoprimo@gmail.com>
2021-02-24 10:37:43 -0300

    Security implementado porém com erro no controller do websecurityconfig

--- src/main/java/com/pontointeligente/api/security/filters/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/pontointeligente/api/security/filters/JwtAuthenticationTokenFilter.java
@@ -1,6 +1,6 @@
 package com.pontointeligente.api.security.filters;
 
-import com.kazale.pontointeligente.api.security.utils.JwtTokenUtil;
+import com.pontointeligente.api.security.utils.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
